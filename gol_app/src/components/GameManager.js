54e05c811fc05901bb12e162ab03af8d5d4fc044
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-17 18:02:15 -0400

    ability to change grid size and ruleSet

--- gol_app/src/components/GameManager.js
+++ gol_app/src/components/GameManager.js
@@ -8,7 +8,11 @@ let intervalId;
 class GameManager extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
+      height: 50,
+      width: 50,
+      ruleSet: "custom",
       matrix: [[]],
       isRunning: false
     };
@@ -38,9 +42,21 @@ class GameManager extends React.Component {
   };
 
   reset = () => {
-    this.setState({ isRunning: false });
+    this.setState({ isRunning: false, matrix: [[]] });
     clearInterval(intervalId);
-    game = new GameOfLife(50, 50, "custom");
+
+    game = new GameOfLife(
+      Number(this.state.height),
+      Number(this.state.height),
+      this.state.ruleSet
+    );
+    this.setState({ matrix: game.matrix });
+  };
+
+  handleChange = ev => {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    });
   };
 
   render() {
@@ -63,6 +79,47 @@ class GameManager extends React.Component {
         <button onClick={this.state.isRunning ? null : this.play}>Play</button>
         <button onClick={this.pause}>Pause</button>
         <button onClick={this.reset}>Reset</button>
+
+        {/* Options */}
+        <div>
+          <h1 style={{ color: "white", margin: "10px" }}>Options</h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "10px"
+            }}
+          >
+            <h3 style={{ color: "white", marginRight: "10px" }}>Grid Size: </h3>
+            <select
+              name="height"
+              value={this.state.height}
+              onChange={this.handleChange}
+            >
+              <option value={30}>30</option>
+              <option value={50}>50</option>
+              <option value={75}>75</option>
+            </select>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "10px"
+            }}
+          >
+            <h3 style={{ color: "white", marginRight: "10px" }}>Rule Set: </h3>
+            <select
+              name="ruleSet"
+              value={this.state.ruleSet}
+              onChange={this.handleChange}
+            >
+              <option value={"custom"}>Custom</option>
+              <option value={"classic"}>Classic</option>
+            </select>
+          </div>
+          <button onClick={this.reset}>Set Options</button>
+        </div>
       </>
     );
   }
