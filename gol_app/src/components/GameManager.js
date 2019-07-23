24a5f7e53928b9bab01d822433ad4226c567446c
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-22 22:38:05 -0400

    moved presets to its own export file

--- gol_app/src/components/GameManager.js
+++ gol_app/src/components/GameManager.js
@@ -6,41 +6,11 @@ import TitleText from "./headers/TitleText";
 import ColorDescriptions from "./headers/ColorDescriptions";
 import Options from "./controlls/Options";
 import Controls from "./controlls/Controlls";
+import presets from "../gol_logic/presets";
 
 let game = new GameOfLife(50, 50, "classic");
 let intervalId;
 
-const preset1 = {
-  size: 50,
-  ruleType: "custom",
-  isolationLimit: 3,
-  suffocationLimit: 6,
-  coords: [[25, 25], [24, 25], [25, 24], [24, 24], [26, 26]]
-};
-
-const preset2 = {
-  size: 20,
-  ruleType: "classic",
-  isolationLimit: 3,
-  suffocationLimit: 6,
-  //prettier-ignore
-  coords: [[6,3],[7,3],[8,3],[12,3],[13,3],[14,3],
-          [4,5],[4,6],[4,7],[4,11],[4,12],[4,13],
-          [9,5],[9,6],[9,7],[9,11],[9,12],[9,13],
-          [11,5],[11,6],[11,7],[11,11],[11,12],[11,13],
-          [16,5],[16,6],[16,7],[16,11],[16,12],[16,13],
-          [6,8],[7,8],[8,8],[12,8],[13,8],[14,8],
-          [6,10],[7,10],[8,10],[12,10],[13,10],[14,10],
-          [6,15],[7,15],[8,15],[12,15],[13,15],[14,15]]
-};
-
-const preset3 = {
-  size: 50,
-  ruleType: "custom",
-  isolationLimit: 2,
-  suffocationLimit: 5,
-  coords: [[25, 25], [24, 25], [25, 24], [24, 24]]
-};
 class GameManager extends React.Component {
   constructor(props) {
     super(props);
@@ -61,7 +31,7 @@ class GameManager extends React.Component {
     // game.randomizeBoard(Number(this.state.seedChance) / 100);
     // this.setState({ matrix: game.matrix });
     // this.play();
-    this.loadPreset(preset3);
+    this.loadPreset(presets[2]);
     this.play();
   }
 
@@ -182,7 +152,6 @@ class GameManager extends React.Component {
           isolationLimit={this.state.isolationLimit}
           seedChance={this.state.seedChance}
           reset={this.reset}
-          state={this.state}
         />
       </ManagerWrapper>
     );
