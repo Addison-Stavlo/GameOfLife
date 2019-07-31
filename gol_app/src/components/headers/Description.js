55340af51acef6071c54553e792bee8fa30c25bf
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-30 23:07:00 -0400

    added GOL description

--- gol_app/src/components/headers/Description.js
+++ gol_app/src/components/headers/Description.js
@@ -15,6 +15,30 @@ export default function Description(props) {
         have given aid to various fields of scientific research including
         biology, physics, and chemistry.{" "}
       </p>
+      <p>
+        One of the most famous implementations of a CA is Conway's Game of Life.
+        Developed by mathematician John Conway in 1970, the Game of Life has
+        been a topic of interest in mathematics and computer science ever since.
+        This universal Turing machine was build with some very simple rules and
+        holds the power to compute anything that can be computed
+        algorithmically.
+      </p>
+      <p>
+        Conway's Rules are:
+        <ul>
+          <li>
+            <span>
+              If a dead cell has exactly 3 live neighbors it will come to life.
+            </span>
+          </li>
+          <li>
+            <span>
+              If a live cell does not have exactly 2 or 3 live neighbors then it
+              dies.
+            </span>
+          </li>
+        </ul>
+      </p>
     </Descriptor>
   );
 }
@@ -24,6 +48,21 @@ const Descriptor = styled.section`
   max-width: 750px;
   line-height: 18px;
   margin: 0 auto;
-  text-align: left;
-  text-indent: 40px;
+
+  p {
+    text-align: left;
+    text-indent: 40px;
+    margin: 15px 0;
+  }
+
+  ul {
+    max-width: 80%;
+    list-style-type: circle;
+    margin: 10px auto;
+
+    span {
+      position: relative;
+      left: -30px;
+    }
+  }
 `;
