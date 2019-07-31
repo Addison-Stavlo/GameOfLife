1c4165dccfbb0026cbb16c143b03c02d777a95e2
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-30 22:37:24 -0400

    started CA description

--- gol_app/src/components/headers/TitleText.js
+++ gol_app/src/components/headers/TitleText.js
@@ -6,6 +6,17 @@ export default function TitleText(props) {
     <Title>
       <h1>Addie's Cellular Automata</h1>
       <p>(& Conway's Game of Life)</p>
+      <p>
+        A Cellular Automaton is discrete mathematical model consisting of a grid
+        of cells whose current state is dependant only on its previous state and
+        a set of predetermined rules. Each generation is calculated by
+        evaluating the amount of active neighbors each cell has. Depending on
+        the amount of active neighbors and the ruleset, a cell may become
+        activated or deactivated. Given a fixed ruleset, the same initial state
+        will evolve in the same manner every time it is run. Cellular Automata
+        have given aid to various fields of scientific research including
+        biology, physics, and chemistry.{" "}
+      </p>
     </Title>
   );
 }
